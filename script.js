d1f82d6096f74a9fe87b9d1fab87ca037ac4b998
Dhaim <mosaab.Dhaim@gmail.com>
2020-08-15 04:01:13 +0200

    Function to exend snake body
    # adding a condition to not pop new apple on snake

--- script.js
+++ script.js
@@ -36,6 +36,16 @@ window.onload = function()
         
         else
         {
+            if(mySnake.isEatingApple(myApple))
+            {
+                mySnake.ateApple = true;
+                do
+                {
+                    myApple.setNewPosition();  
+                }
+                while (myApple.isOnSnake(mySnake))
+            }
+            
             ctx.clearRect( 0 , 0 , canvasWidth , canvasHeight);
             mySnake.draw();
             myApple.draw();
@@ -54,6 +64,7 @@ window.onload = function()
     {
         this.body = body;
         this.direction = direction;
+        this.ateApple = false;
         
         this.draw = function()
         {
@@ -86,8 +97,13 @@ window.onload = function()
                 default:
                     throw("Invalid direction");
             }
+            
             this.body.unshift(nextPosition);
-            this.body.pop();
+            
+            if(!this.ateApple)
+                this.body.pop();
+            else 
+                this.ateApple = false;
         };
         
         this.setDirection = function(newDirection)
@@ -143,6 +159,13 @@ window.onload = function()
             }
             return wallCollision || snakeCollision; 
         };
+        
+        this.isEatingApple = function(appleToEat)
+        {
+            var head = this.body[0];
+            if( head[0] === appleToEat.position[0] && head[1] === appleToEat.position[1]) return true;
+            else return false;
+        };
                     
     }
     
@@ -155,11 +178,31 @@ window.onload = function()
             ctx.fillStyle = "#33cc33";
             ctx.beginPath();
             var radius = blockSize/2;
-            var x = position[0] * blockSize + radius;
-            var y = position[1] * blockSize + radius;
+            var x = this.position[0] * blockSize + radius;
+            var y = this.position[1] * blockSize + radius;
             ctx.arc( x, y, radius, 0, Math.PI*2, true);
             ctx.fill();
             ctx.restore();
+        };
+        
+        this.setNewPosition = function()
+        {
+            var newX = Math.round(Math.random() * (widthInBlocks - 1));
+            var newY = Math.round(Math.random() * (heightInBlocks - 1));
+            this.position = [ newX, newY ];
+        };
+        
+        this.isOnSnake = function(snakeToCheck)
+        {
+            var isOnSnake = false;
+            for( var i =0; i < snakeToCheck.body.length;i++)
+            {
+                if(this.position[0] === snakeToCheck.body[i][0] &&  this.position[1] === snakeToCheck.body[i][1])
+                {
+                    isOnSnake = true;
+                }
+            }
+            return isOnSnake;
         };
     }
                     
